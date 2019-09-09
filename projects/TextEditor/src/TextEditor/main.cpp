d85de0f2550a963387dc4b2aee2ca22db17dbd80
Brian Corbett <bricor14@gmail.com>
2019-09-08 20:21:53 -0700

    menu toolbar is shown with a couple options. also prints characters that are typed on the keyboard.

--- projects/TextEditor/src/TextEditor/main.cpp
+++ projects/TextEditor/src/TextEditor/main.cpp
@@ -15,9 +15,26 @@ int main(int argc, char ** argv)
 	noecho();
 	keypad(stdscr, TRUE);
 
+
+	mvhline(0, 0, '-', COLS);
+	mvhline(2, 0, '-', COLS);
+	int opt_width = 7;
+	for (int i = 0; i < 21; i++)
+	{
+		mvvline(1, opt_width * i, '|', 1);
+	}
+
+	string menu_options[3] = { "File", "Edit", "View" };
+	for (int i = 0; i < 3; i++)
+	{
+		mvprintw(1, (opt_width * i) + 2, menu_options[i].c_str());
+	}
+
 	int cursor_x = 0;
-	int cursor_y = 0;
+	int cursor_y = 3;
 	move(cursor_y, cursor_x);
+
+	refresh();
 	
 	while (true)
 	{
@@ -46,7 +63,16 @@ int main(int argc, char ** argv)
 		else
 		{
 			if (cursor_x < COLS - 1)
+			{
+				addch(c);
 				move(cursor_y, ++cursor_x);
+			}
+			else
+			{
+				cursor_x = 0;
+				move(++cursor_y, cursor_x);
+				addch(c);
+			}
 		}
 	}
 
