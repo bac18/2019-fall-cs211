112d013862dc29f42e43b7f3218498bc0080d36a
Brian Corbett <bricor14@gmail.com>
2019-09-10 20:22:55 -0700

    Final updates before release

--- projects/TextEditor/docs/milestone1_design_diary.md
+++ projects/TextEditor/docs/milestone1_design_diary.md
@@ -1,2 +1,10 @@
-# Design Diary
-Use this space to talk about your process.  For inspiration, see [my prompts](../../../docs/sample_reflection.md) 
+The beginning of the project was particularly frustrating. Getting curses.h to work was as struggle. However, it did
+remind me of the pain and struggle of not understanding what is wrong and the problem is right in front of your face.
+After getting it to work it was a lot of fun to begin coding a terminal window text editor.
+I was never quite able to figure out how to make each option in the options bar in it's own window.
+I would really recommend asking questions as soon as they pop up in the head. This is because it is quite
+easy to fall behind even in such a seemingly simple assignment and it's worth getting a lot of practice with it.
+The most challenging aspect of the assignment was staying patient with the language. The way curses works is different
+than anything else I've used in C++. 
+Another challenge was not getting ahead of myself. I found that I have many big ideas for the text editor, but because
+of lack of experience and time, I had to hold myself back many times. 
--- projects/TextEditor/src/TextEditor/main.cpp
+++ projects/TextEditor/src/TextEditor/main.cpp
@@ -11,34 +11,51 @@ using namespace std;
 
 int main(int argc, char ** argv)
 {
-	WINDOW* main_window = nullptr;
+	WINDOW* main_win = nullptr;
 
-	main_window = initscr();				// initializes the screen, sets up memory
+	main_win = initscr();				// initializes the screen, sets up memory
 	start_color();
 	noecho();
+	keypad(main_win, TRUE);
+
+	WINDOW* main_window = newwin(LINES - 3, COLS, 3, 0);
 	keypad(main_window, TRUE);
-	keypad(stdscr, TRUE);
+	wborder(main_window, ACS_VLINE, ACS_VLINE, ACS_HLINE, ACS_HLINE, 
+		ACS_ULCORNER, ACS_URCORNER, ACS_LLCORNER, ACS_LRCORNER);
+	refresh();
+	wrefresh(main_window);
+
+	WINDOW* options_bar_wind = newwin(3, COLS, 0, 0);
+	keypad(options_bar_wind, TRUE);
+	wborder(options_bar_wind, ACS_VLINE, ACS_VLINE, ACS_HLINE, ACS_HLINE,
+		ACS_ULCORNER, ACS_URCORNER, ACS_LLCORNER, ACS_LRCORNER);
+	refresh();
+	wrefresh(options_bar_wind);
 
 	int opt_height = 1;
 	int opt_width = 10;
-	mvhline(0, 0, '-', COLS);
-	mvhline(2, 0, '-', COLS);
+	/*mvhline(0, 1, '-', COLS - 1);
+	mvhline(2, 1, '=', COLS - 1);*/
 	for (int i = 0; i < 21; i++)
 		mvvline(opt_height, opt_width * i, '|', 1);
+	/*mvvline(0, 0, '|', LINES);*/
+
 
 	const int num_of_options = 5;
 	string menu_options[num_of_options] = { "File", "Edit", "View", "Format", "Help" };
 
 	for (int i = 0; i < num_of_options; i++)
 	{
+		init_pair(2, COLOR_BLACK, COLOR_WHITE);
+		attron(COLOR_PAIR(2));
 		mvprintw(1, (opt_width * i) + (opt_width / 3), menu_options[i].c_str());
+		attroff(COLOR_PAIR(2));
 		refresh();
 	}
 
-	int cursor_x = 0;
-	int cursor_y = 3;
+	int cursor_x = 1;
+	int cursor_y = 4;
 	move(cursor_y, cursor_x);
-
 	refresh();
 
 	while (true)
@@ -47,12 +64,12 @@ int main(int argc, char ** argv)
 
 		if (c == KEY_RIGHT)
 		{
-			if (cursor_x < COLS - 1)
+			if (cursor_x < COLS - 2)
 				move(cursor_y, ++cursor_x);
 		}
 		else if (c == KEY_LEFT)
 		{
-			if (cursor_x > 0)
+			if (cursor_x > 1)
 				move(cursor_y, --cursor_x);
 		}
 		else if (c == KEY_DOWN)
@@ -62,18 +79,18 @@ int main(int argc, char ** argv)
 		}
 		else if (c == KEY_UP)
 		{
-			if (cursor_y > 3)
+			if (cursor_y > 4)
 				move(--cursor_y, cursor_x);
 		}
-		else if (c == KEY_BACKSPACE)
+		else if (c == KEY_DC)
 		{
-			move(cursor_y, --cursor_x);
-			addch('<');
-			refresh();
+			if (cursor_x > 1)
+				move(cursor_y, --cursor_x);
+			delch();
 		}
 		else
 		{
-			if (cursor_x < COLS - 1)
+			if (cursor_x < COLS - 2)
 			{
 				addch(c);
 				move(cursor_y, ++cursor_x);
@@ -84,7 +101,6 @@ int main(int argc, char ** argv)
 				move(++cursor_y, cursor_x);
 				addch(c);
 			}
-
 		refresh();
 		}
 	}
